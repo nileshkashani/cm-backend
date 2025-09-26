5333561c8eb054d7830ad62b119c7646837cd32d
nilesh <nileshkashani45@gmail.com>
2025-09-26 19:29:33 +0530

    updated backend

--- src/main/java/com/clubmatrix/demo/config/SecurityConfig.java
+++ src/main/java/com/clubmatrix/demo/config/SecurityConfig.java
@@ -1,3 +1,17 @@
+package com.clubmatrix.demo.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
